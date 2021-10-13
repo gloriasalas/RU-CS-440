84946f1e21bbe1b9efd39f4ae7a67ef112027fc7
Yulin Ni <pocketyulin@gmail.com>
2021-10-13 14:31:16 -0400

    update

--- Assignments/Homework/Submission/Code.py
+++ Assignments/Homework/Submission/Code.py
@@ -1,51 +1,58 @@
 import random
 
 class Square:
-    def __init__(self, visited, blocked, isAgent):
+    def __init__(self, visited, blocked):
         self.visited = visited
         self.blocked = blocked
         self.isAgent = False
+        # print(self.blocked)
 
 # class Grid(Square):
 #     def __init__(self):
 #         grid = [[Square(False, False) for x in range(101)] for y in range(101)]
 
-grid = [[Square(False, False, False) for x in range(101)] for y in range(101)]
-
 #iterate through grid_list and assign all squares as blocked or unblocked with a 3:7 ratio
 
-grid_list = []
+grid_list = [] #list of 50 grids
 for i in range(50):
-    grid_list.append(grid)
+    grid_list.append([[Square(False, False) for x in range(10)] for y in range(10)])
 
-stack = []
+# stack = []
 
 for grid in grid_list:
     starting_square = grid[0][0]
-    ending_square = grid[100][100]
     starting_square.visited = True
     starting_square.isAgent = True
-    for i in range(101):
-        for square in grid[i]:
-            if not(grid[i].index(square) == 0 and i == 0) and not(grid[i].index(square) == 101 and i == 101):
-                n = random.randrange(1, 100)
-                if n > 70:
-                    square.blocked = True
-                # print(square.blocked)
+    for i in range(10):
+        for j in range(10):
+            if not(i == 0 and j == 0) and not(i == 9 and j == 9):
+                n = random.randrange(1, 10)
+                # print(grid[i][j].blocked)
+                if n > 7:
+                    grid[i][j].blocked = True
+                print(grid[i][j].blocked)
 
 def draw_grid(grid):
-    for i in range(101):
+    for i in range(10):
+        empty_string = ""
         for square in grid[i]:
             if square.isAgent:
-                print("A")
-            elif grid[i].index(square) == 101 and i == 101:
-                print("T")
+                # print("A")
+                empty_string += "A"
+            elif grid[i].index(square) == 9 and i == 9:
+                empty_string += "T"
             elif square.blocked == True:
-                print("X")
+                # print("X")
+                empty_string += "X"
             elif square.blocked == False:
-                print(" ")
-            if grid[i].index(square) == 101:
-                print("\n")
+                # print(" ")
+                empty_string += " "
+        if grid[i].index(square) == 9:
+            # print("\n")
+            empty_string += "\n"
+        print(empty_string)
 
 draw_grid(grid_list[0])
+
+#set variable for gridsize
 
